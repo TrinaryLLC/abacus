7067cb492d2f00736e12899b9a34849ba5f0c5ab
aaron.awilliams@gmail.com <aaron.awilliams@gmail.com>
2024-07-01 23:40:27 -0400

    updated strategies to contain HREF

--- app/ui.py
+++ app/ui.py
@@ -13,12 +13,22 @@ app.mount("/static", StaticFiles(directory="app/static"), name="static")
 # Strategy list with names and descriptions. Able to select a strategy.
 @app.get("/")
 async def read_root(request: Request):
-    return templates.TemplateResponse("page/index.html", {"request": request, "title": "My FastAPI App"})
+    strategies = db.get_strategies()
+    return templates.TemplateResponse("page/index.html", {"request": request, "title": "My FastAPI App","strategies": strategies})
 
 @app.get("/strategies")
 async def read_strategies(request: Request):
     strategies = db.get_strategies()
     return templates.TemplateResponse("partial/strategy_selector.html", {"request": request, "strategies": strategies})
+
+## STRATEGY VIEWER
+# Show strategy information
+# Show baskets on right side
+# Show instruments on bottom
+@app.get("/strategies/{strategy_id}")
+async def read_strategy(request: Request, strategy_id: int):
+    strategy = db.get_strategy_by_id(strategy_id)
+    return templates.TemplateResponse("page/strategy.html", {"request": request, "strategy": strategy})
 
 if __name__ == "__main__":
     import uvicorn
